3b1954eef4e3c962d0d2e6f6f2c2bc84c534221f
BilberryA <nastya.ikonnikova.01@gmail.com>
2022-03-12 15:56:59 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/misc.xml
    #	Lab/src/main/java/com/example/Lab/LoginActivity.java
    #	Lab/src/main/java/com/example/Lab/UserActivity.java

--- Lab/src/main/java/com/example/Lab/UserActivity.java
+++ Lab/src/main/java/com/example/Lab/UserActivity.java
@@ -6,10 +6,14 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
@@ -17,12 +21,14 @@ import android.widget.TextView;
 public class UserActivity extends AppCompatActivity implements View.OnClickListener{
 
     Button BackBtn;
-    EditText etName, etService, etResult, etDate, etPrice;
+    EditText etName, etService, etResult, etDate, etPrice, search;
+    Spinner filtrSpinner, sortSpinner;
 
     DBHelper dbHelper;
     SQLiteDatabase database;
 
-    String name;
+    String name, fil, sor, sear;
+    String[] filtr, sortir;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +43,51 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
         etResult = (EditText) findViewById(R.id.result);
         etDate = (EditText) findViewById(R.id.date);
         etPrice = (EditText) findViewById(R.id.price);
+        filtrSpinner = findViewById(R.id.filtrSpinner);
+        sortSpinner = findViewById(R.id.sortSpinner);
+        search = (EditText) findViewById(R.id.search);
+        sear = "";
+        search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                if ((keyEvent != null && KeyEvent.KEYCODE_ENTER == keyEvent.getKeyCode() && keyEvent.getAction() == KeyEvent.ACTION_DOWN)) {
+                    sear = search.getText().toString();
+                    UpdateTable();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        filtrSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                filtr = getResources().getStringArray(R.array.filtr);
+                fil = filtr[i];
+                UpdateTable();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+        sortSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                sortir = getResources().getStringArray(R.array.sortir);
+                sor = sortir[i];
+                UpdateTable();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
 
         dbHelper = new DBHelper(this);
         database = dbHelper.getWritableDatabase();
@@ -49,58 +100,128 @@ public class UserActivity extends AppCompatActivity implements View.OnClickListe
 
     public void UpdateTable() {
         Cursor cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ?", new String[]{name}, null, null, null);
-
-        if (cursor.moveToFirst()) {
-            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID_RESULT);
-            int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME2);
-            int serviceIndex = cursor.getColumnIndex(DBHelper.KEY_SERVICE);
-            int resultIndex = cursor.getColumnIndex(DBHelper.KEY_RESULT);
-            int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
-            int priceIndex = cursor.getColumnIndex(DBHelper.KEY_PRICE);
-            TableLayout dbOutput = findViewById(R.id.output3);
-            dbOutput.removeAllViews();
-            do {
-                TableRow dbOutputRow = new TableRow(this);
-                dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-
-                TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
-
-                TextView outputName = new TextView(this);
-                params.weight = 5.0f;
-                outputName.setLayoutParams(params);
-                outputName.setText(cursor.getString(nameIndex));
-                dbOutputRow.addView(outputName);
-
-                TextView outputService = new TextView(this);
-                params.weight = 5.0f;
-                outputService.setLayoutParams(params);
-                outputService.setText(cursor.getString(serviceIndex));
-                dbOutputRow.addView(outputService);
-
-                TextView outputResult = new TextView(this);
-                params.weight = 2.0f;
-                outputResult.setLayoutParams(params);
-                outputResult.setText(cursor.getString(resultIndex));
-                dbOutputRow.addView(outputResult);
-
-                TextView outputDate = new TextView(this);
-                params.weight = 4.0f;
-                outputDate.setLayoutParams(params);
-                outputDate.setText(cursor.getString(dateIndex));
-                dbOutputRow.addView(outputDate);
-
-                TextView outputPrice = new TextView(this);
-                params.weight = 3.0f;
-                outputPrice.setLayoutParams(params);
-                outputPrice.setText(cursor.getString(priceIndex));
-                dbOutputRow.addView(outputPrice);
-
-                dbOutput.addView(dbOutputRow);
-
-            } while (cursor.moveToNext());
+        if (!sear.equals("")) {
+            if (fil != null) {
+                if (fil.equals(getResources().getStringArray(R.array.filtr)[1])) {
+                    if (sor != null) {
+                        if (sor.equals(getResources().getStringArray(R.array.sortir)[1])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, "+", sear}, null, null, DBHelper.KEY_DATE + " ASC");
+                        } else if (sor.equals(getResources().getStringArray(R.array.sortir)[2])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, "+", sear}, null, null, DBHelper.KEY_DATE + " DESC");
+                        } else {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, "+", sear}, null, null, null);
+                        }
+                    }
+                } else if (fil.equals(getResources().getStringArray(R.array.filtr)[2])) {
+                    if (sor != null) {
+                        if (sor.equals(getResources().getStringArray(R.array.sortir)[1])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, "-", sear}, null, null, DBHelper.KEY_DATE + " ASC");
+                        } else if (sor.equals(getResources().getStringArray(R.array.sortir)[2])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, "-", sear}, null, null, DBHelper.KEY_DATE + " DESC");
+                        } else {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, "-", sear}, null, null, null);
+                        }
+                    }
+                } else {
+                    if (sor != null) {
+                        if (sor.equals(getResources().getStringArray(R.array.sortir)[1])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, sear}, null, null, DBHelper.KEY_DATE + " ASC");
+                        } else if (sor.equals(getResources().getStringArray(R.array.sortir)[2])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, sear}, null, null, DBHelper.KEY_DATE + " DESC");
+                        } else {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_SERVICE + " = ?", new String[]{name, sear}, null, null, null);
+                        }
+                    }
+                }
+            }
         }
-        cursor.close();
-    }
+        else if (sear.equals("")) {
+            if (fil != null) {
+                if (fil.equals(getResources().getStringArray(R.array.filtr)[1])) {
+                    if (sor != null) {
+                        if (sor.equals(getResources().getStringArray(R.array.sortir)[1])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ?", new String[]{name, "+"}, null, null, DBHelper.KEY_DATE + " ASC");
+                        } else if (sor.equals(getResources().getStringArray(R.array.sortir)[2])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ?", new String[]{name, "+"}, null, null, DBHelper.KEY_DATE + " DESC");
+                        } else {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ?", new String[]{name, "+"}, null, null, null);
+                        }
+                    }
+                } else if (fil.equals(getResources().getStringArray(R.array.filtr)[2])) {
+                    if (sor != null) {
+                        if (sor.equals(getResources().getStringArray(R.array.sortir)[1])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ?", new String[]{name, "-"}, null, null, DBHelper.KEY_DATE + " ASC");
+                        } else if (sor.equals(getResources().getStringArray(R.array.sortir)[2])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ?", new String[]{name, "-"}, null, null, DBHelper.KEY_DATE + " DESC");
+                        } else {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ? AND " + DBHelper.KEY_RESULT + " = ?", new String[]{name, "-"}, null, null, null);
+                        }
+                    }
+                } else {
+                    if (sor != null) {
+                        if (sor.equals(getResources().getStringArray(R.array.sortir)[1])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ?", new String[]{name}, null, null, DBHelper.KEY_DATE + " ASC");
+                        } else if (sor.equals(getResources().getStringArray(R.array.sortir)[2])) {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ?", new String[]{name}, null, null, DBHelper.KEY_DATE + " DESC");
+                        } else {
+                            cursor = database.query(DBHelper.TABLE_RESULTS, null, DBHelper.KEY_NAME2 + " = ?", new String[]{name}, null, null, null);
+                        }
+                    }
+                }
+            }
+        }
+
+            if (cursor.moveToFirst()) {
+                int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID_RESULT);
+                int nameIndex = cursor.getColumnIndex(DBHelper.KEY_NAME2);
+                int serviceIndex = cursor.getColumnIndex(DBHelper.KEY_SERVICE);
+                int resultIndex = cursor.getColumnIndex(DBHelper.KEY_RESULT);
+                int dateIndex = cursor.getColumnIndex(DBHelper.KEY_DATE);
+                int priceIndex = cursor.getColumnIndex(DBHelper.KEY_PRICE);
+                TableLayout dbOutput = findViewById(R.id.output3);
+                dbOutput.removeAllViews();
+                do {
+                    TableRow dbOutputRow = new TableRow(this);
+                    dbOutputRow.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+
+                    TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT);
+
+                    TextView outputName = new TextView(this);
+                    params.weight = 5.0f;
+                    outputName.setLayoutParams(params);
+                    outputName.setText(cursor.getString(nameIndex));
+                    dbOutputRow.addView(outputName);
+
+                    TextView outputService = new TextView(this);
+                    params.weight = 5.0f;
+                    outputService.setLayoutParams(params);
+                    outputService.setText(cursor.getString(serviceIndex));
+                    dbOutputRow.addView(outputService);
+
+                    TextView outputResult = new TextView(this);
+                    params.weight = 2.0f;
+                    outputResult.setLayoutParams(params);
+                    outputResult.setText(cursor.getString(resultIndex));
+                    dbOutputRow.addView(outputResult);
+
+                    TextView outputDate = new TextView(this);
+                    params.weight = 4.0f;
+                    outputDate.setLayoutParams(params);
+                    outputDate.setText(cursor.getString(dateIndex));
+                    dbOutputRow.addView(outputDate);
+
+                    TextView outputPrice = new TextView(this);
+                    params.weight = 3.0f;
+                    outputPrice.setLayoutParams(params);
+                    outputPrice.setText(cursor.getString(priceIndex));
+                    dbOutputRow.addView(outputPrice);
+
+                    dbOutput.addView(dbOutputRow);
+
+                } while (cursor.moveToNext());
+            }
+            cursor.close();
+        }
 
     @Override
     public void onClick(View v) {
